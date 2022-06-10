f9a7768cf7f00c8d5d015eacb994e17968a90d37
christopherchinia <chinia214@gmail.com>
2022-06-10 08:56:00 -0400

    commit tree 99ead6f211af26fbf50371fe73491b2de28f77b2

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+customerName = "bob";
+console.log(customerName);
+
+function upperCaseCustomerName(){
+    customerName = "BOB";
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+} 
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "You are my least favorite customer"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "You shouldnot be here. ";
+}
